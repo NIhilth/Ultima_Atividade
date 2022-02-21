538e7225520b024fc690cae8e7f07cb46100094b
NIhilth <91199065+NIhilth@users.noreply.github.com>
2022-02-21 15:43:45 -0300

    tentativa verificação de pessoa

--- Ultimo projeto/gerenciaNotas/src/app/checkLoggedProfessor.canActivate.ts
+++ Ultimo projeto/gerenciaNotas/src/app/checkLoggedProfessor.canActivate.ts
@@ -6,11 +6,13 @@ import {
     RouterStateSnapshot
 } from "@angular/router";
 import { Observable } from "rxjs";
+import { UsuarioService } from "./services/usuario.service";
 
 @Injectable()
 class CheckLoggedProfessor implements CanActivate {
     constructor(
-        private router: Router
+        private router: Router,
+        private usuarioService: UsuarioService
     ) { }
 
     canActivate(
@@ -25,13 +27,32 @@ class CheckLoggedProfessor implements CanActivate {
 
         if (user && password) {
             if (verifica == '1') {
+                this.usuarioService.dadosPessoa()
+                .then((resultado: (Object: (String|boolean)) => [] ) => {
+                    for(let i = 0; i < resultado.length; i++){
+                        if(resultado[i].USUARIO === user && resultado[i].SENHA === password){
+                            this.usuarioService.dadosProfessor()
+                            .then((result: (Object: (String)) =>[]) => {
+                                for(let j = 0; j< result.length; j++){
+                                    if(id == result[j].ID){
+                                        console.log("aaaa")
+                                        return true
+                                    }
+                                }
+                                console.log('hum')
+                            })
+                        }
+                    }
+                    console.log('blebleb')
+                })
                 console.log('foi checked')
-                return true;
             } else {
+                console.log("a meu caralho")
                 this.router.navigate([''])
                 return false;
             }
         } else {
+            console.log("pqp")
             this.router.navigate([''])
             return false;
         }
